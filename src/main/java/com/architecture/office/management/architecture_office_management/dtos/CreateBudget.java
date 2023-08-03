51666280bcafe0855e65bd700b2259e94fc1a294
Carla Hentschel <carlarhentschel@gmail.com>
2023-08-03 15:45:53 -0300

    cria dto para criar orçamento

---
+++ src/main/java/com/architecture/office/management/architecture_office_management/dtos/CreateBudget.java
@@ -0,0 +1,31 @@
+package com.architecture.office.management.architecture_office_management.dtos;
+
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.Length;
+import java.time.LocalDate;
+
+public record CreateBudget(
+        @NotBlank
+        @Length(min = 3, max = 100)
+        String customerName,
+        @NotBlank
+        @Length(min = 3, max = 100)
+        String project,
+        @NotBlank
+        @Length(min = 3)
+        String projectStages,
+        @NotBlank
+        @Length(min = 3, max = 100)
+        String workAddress,
+        @NotBlank
+        int estimedHours,
+        @NotNull
+        @FutureOrPresent
+        LocalDate date,
+        @NotNull
+        double value
+) {
+}
