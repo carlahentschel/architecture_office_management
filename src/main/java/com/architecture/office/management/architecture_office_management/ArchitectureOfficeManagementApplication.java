a11e9d608e720641aa399aa65efe6691ba8efcb0
Carla Hentschel <carlarhentschel@gmail.com>
2023-08-03 14:17:57 -0300

    primeiro commit

---
+++ src/main/java/com/architecture/office/management/architecture_office_management/ArchitectureOfficeManagementApplication.java
@@ -0,0 +1,14 @@
+package com.architecture.office.management.architecture_office_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchitectureOfficeManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchitectureOfficeManagementApplication.class, args);
+	}
+
+
+}
---
+++ src/test/java/com/architecture/office/management/architecture_office_management/ArchitectureOfficeManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.architecture.office.management.architecture_office_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchitectureOfficeManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
