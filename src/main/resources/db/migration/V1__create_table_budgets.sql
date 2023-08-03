21c57cb4d0cb63815db37178500079b3e360fbf2
Carla Hentschel <carlarhentschel@gmail.com>
2023-08-03 15:24:27 -0300

    refatora nome do arquivo

--- src/main/resources/db/migration/V1__create_table_budgets.sql
+++ src/main/resources/db/migration/V1__create_table_budgets.sql
@@ -6,5 +6,7 @@ create table budgets(
     work_address varchar(100) not null,
     estimated_hours int not null,
     "date" date not null,
-    "value" double not null
+    "value" decimal not null,
+    created_at date not null default now(),
+    updated_at timestamp not null default now()
 );
