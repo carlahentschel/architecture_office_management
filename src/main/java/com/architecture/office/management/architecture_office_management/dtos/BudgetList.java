8fcb7d384d1ff40f58a5cb36bc2abb102783a276
Carla Hentschel <carlarhentschel@gmail.com>
2023-08-07 17:52:27 -0300

    cria dto para listar orçamentos

---
+++ src/main/java/com/architecture/office/management/architecture_office_management/dtos/BudgetList.java
@@ -0,0 +1,37 @@
+package com.architecture.office.management.architecture_office_management.dtos;
+
+import com.architecture.office.management.architecture_office_management.models.Budget;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+public record BudgetList(
+        UUID id,
+        String customerName,
+        String project,
+        String projectStages,
+        int squareMeters,
+        String workAddress,
+        int estimedHours,
+        LocalDate date,
+        double value
+        //int version
+) {
+
+    public BudgetList(Budget budget) {
+        this(
+                budget.getId(),
+                budget.getCustomerName(),
+                budget.getProject(),
+                budget.getProjectStages(),
+                budget.getSquareMeters(),
+                budget.getWorkAddress(),
+                budget.getEstimedHours(),
+                budget.getDate(),
+                budget.getValue()
+                //budget.getVersion()
+        );
+    }
+
+
+}
