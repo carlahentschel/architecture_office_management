fd9a2f9666e31e6b24c5a259589f6fd4adb04927
Carla Hentschel <carlarhentschel@gmail.com>
2023-08-09 20:32:06 -0300

    cria dto para criar contrato

---
+++ src/main/java/com/architecture/office/management/architecture_office_management/dtos/CreateContract.java
@@ -0,0 +1,23 @@
+package com.architecture.office.management.architecture_office_management.dtos;
+
+import com.architecture.office.management.architecture_office_management.models.Budget;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDate;
+
+
+public record CreateContract(
+        @NotBlank
+        String formOfPayment,
+        @NotNull
+        LocalDate initialDate,
+        @NotNull
+        LocalDate finalDate,
+        @NotNull
+        Budget budget,
+        @NotBlank
+        String customerCpf
+
+) {
+}
