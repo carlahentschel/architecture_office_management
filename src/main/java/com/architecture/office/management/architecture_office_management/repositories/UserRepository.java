1abc6e381f9266662a31d8ce99858b29dc4abd06
Carla Hentschel <carlarhentschel@gmail.com>
2023-08-09 20:41:56 -0300

    cria repositório para "usuário"

---
+++ src/main/java/com/architecture/office/management/architecture_office_management/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.architecture.office.management.architecture_office_management.repositories;
+
+import com.architecture.office.management.architecture_office_management.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+    UserDetails findByUsername(String username);
+}
