78d890c95b28835592b8c734b0f147cd67581ead
Carla Hentschel <carlarhentschel@gmail.com>
2023-08-10 13:29:31 -0300

    adiciona métodos sql

--- src/main/java/com/architecture/office/management/architecture_office_management/repositories/UserRepository.java
+++ src/main/java/com/architecture/office/management/architecture_office_management/repositories/UserRepository.java
@@ -3,10 +3,12 @@ package com.architecture.office.management.architecture_office_management.reposi
 import com.architecture.office.management.architecture_office_management.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
-
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
 
     UserDetails findByUsername(String username);
+    UserDetails getReferenceUserById(UUID id);
+    Boolean existsByUsername(String username);
+
 }
