9b9cee87022b633442a2b1b45fbf1bacfead1e1e
Carla Hentschel <carlarhentschel@gmail.com>
2023-08-11 17:22:12 -0300

    cria query para somar pagamentos

--- src/main/java/com/architecture/office/management/architecture_office_management/repositories/PaymentRepository.java
+++ src/main/java/com/architecture/office/management/architecture_office_management/repositories/PaymentRepository.java
@@ -2,7 +2,21 @@ package com.architecture.office.management.architecture_office_management.reposi
 
 import com.architecture.office.management.architecture_office_management.models.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import java.time.LocalDate;
 import java.util.UUID;
 
 public interface PaymentRepository extends JpaRepository<Payment, UUID> {
+    @Query(value = """
+            select
+                sum(p.value)
+            from
+                payments p
+            where
+                p.date >= :startDate and p.date <= :endDate
+            ;
+            """,nativeQuery = true)
+    double sumPaymentsInDateRange(@Param("startDate") LocalDate startDate, @Param("endDate") LocalDate endDate);
+
 }
