e99407f3b16fe1a55dcac1449639881f24784241
Carla Hentschel <carlarhentschel@gmail.com>
2023-08-11 17:19:49 -0300

    refatora query

--- src/main/java/com/architecture/office/management/architecture_office_management/repositories/ContractRepository.java
+++ src/main/java/com/architecture/office/management/architecture_office_management/repositories/ContractRepository.java
@@ -2,8 +2,21 @@ package com.architecture.office.management.architecture_office_management.reposi
 
 import com.architecture.office.management.architecture_office_management.models.Contract;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import java.time.LocalDate;
 import java.util.UUID;
 
 public interface ContractRepository extends JpaRepository<Contract, UUID> {
+    @Query(value = """
+            select
+                count(c.*) as total_contracts
+            from
+                contracts c
+            where
+                signature_date >= :startDate and signature_date <= :endDate
+            ;
+            """,nativeQuery = true)
+    int countContractsInDateRange(@Param("startDate") LocalDate startDate, @Param("endDate") LocalDate endDate);
+
 }
