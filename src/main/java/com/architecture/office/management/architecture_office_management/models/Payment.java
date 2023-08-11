6e1044ecfe3e04d69e01174dde19a4835c3e5bf8
Carla Hentschel <carlarhentschel@gmail.com>
2023-08-10 21:04:40 -0300

    cria classe de payment

---
+++ src/main/java/com/architecture/office/management/architecture_office_management/models/Payment.java
@@ -0,0 +1,31 @@
+package com.architecture.office.management.architecture_office_management.models;
+
+import com.architecture.office.management.architecture_office_management.dtos.CreatePayment;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import java.time.LocalDate;
+import java.util.UUID;
+
+@NoArgsConstructor
+@Getter
+@Entity
+@Table(name = "payments")
+public class Payment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private double value;
+    private String quota;
+    private LocalDate date;
+    @OneToOne
+    private Contract contract;
+
+    public Payment(CreatePayment newPayment) {
+        value = newPayment.value();
+        quota = newPayment.quota();
+        date = newPayment.date();
+        contract = newPayment.contract();
+    }
+
+}
