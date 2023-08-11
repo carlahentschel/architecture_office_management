55df083b39ead571bb4ff09badd62336af3b42be
Carla Hentschel <carlarhentschel@gmail.com>
2023-08-10 21:00:08 -0300

    adiciona método sql

--- src/main/java/com/architecture/office/management/architecture_office_management/repositories/BudgetRepository.java
+++ src/main/java/com/architecture/office/management/architecture_office_management/repositories/BudgetRepository.java
@@ -1,8 +1,24 @@
 package com.architecture.office.management.architecture_office_management.repositories;
 
+import com.architecture.office.management.architecture_office_management.dtos.ContractsByBudgets;
 import com.architecture.office.management.architecture_office_management.models.Budget;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
 import java.util.UUID;
 
 public interface BudgetRepository extends JpaRepository<Budget, UUID> {
+
+    @Query(value = """
+            select
+                count(b.*) as qtBudgets
+            from
+                budgets b
+            left join
+                contracts c
+            on
+                b.id = c.budget_id
+            ;
+            """,nativeQuery = true)
+    int calculateBudgets();
 }
