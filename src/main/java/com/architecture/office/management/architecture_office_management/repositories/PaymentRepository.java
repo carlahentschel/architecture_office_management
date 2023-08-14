38331ac7b2c1897e5ce8c86b0d2728b9af78938b
Carla Hentschel <carlarhentschel@gmail.com>
2023-08-14 17:47:13 -0300

    adiciona query para buscar pagamentos por período

--- src/main/java/com/architecture/office/management/architecture_office_management/repositories/PaymentRepository.java
+++ src/main/java/com/architecture/office/management/architecture_office_management/repositories/PaymentRepository.java
@@ -1,10 +1,14 @@
 package com.architecture.office.management.architecture_office_management.repositories;
 
+import com.architecture.office.management.architecture_office_management.dtos.PaymentList;
 import com.architecture.office.management.architecture_office_management.models.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+
+import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.List;
 import java.util.UUID;
 
 public interface PaymentRepository extends JpaRepository<Payment, UUID> {
@@ -18,5 +22,18 @@ public interface PaymentRepository extends JpaRepository<Payment, UUID> {
             ;
             """,nativeQuery = true)
     double sumPaymentsInDateRange(@Param("startDate") LocalDate startDate, @Param("endDate") LocalDate endDate);
+
+    @Query(value = """
+            select
+                p.*
+            from
+                payments p
+            where
+                p.date >= :startDate and p.date <= :endDate
+            ;
+            """,nativeQuery = true)
+    List<Payment> paymentsInDateRange(@Param("startDate") LocalDate startDate, @Param("endDate") LocalDate endDate);
+
+
 
 }
