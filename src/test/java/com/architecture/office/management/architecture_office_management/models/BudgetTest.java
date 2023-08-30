a63c63ababd03b04cded0a788426545162f8dfea
Carla Hentschel <carlarhentschel@gmail.com>
2023-08-30 12:37:53 -0300

    adiciona mais um teste unitário para atualizar orçamento

--- src/test/java/com/architecture/office/management/architecture_office_management/models/BudgetTest.java
+++ src/test/java/com/architecture/office/management/architecture_office_management/models/BudgetTest.java
@@ -4,11 +4,11 @@ import com.architecture.office.management.architecture_office_management.builder
 import com.architecture.office.management.architecture_office_management.builders.models.BudgetBuilder;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import java.time.LocalDate;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class BudgetTest {
 
-    // não está passando
     @Test
     @DisplayName("Deve atualizar customerName, project, projectStages, squareMeters, workAddress, estimedHours, date e value")
     void updateBudgetCase1() {
@@ -30,4 +30,34 @@ class BudgetTest {
         assertThat(budget.getDate()).isEqualTo(dataToUpdate.date());
         assertThat(budget.getValue()).isEqualTo(dataToUpdate.value().get());
     }
+
+    @Test
+    @DisplayName("Deve atualizar squareMeters, estimedHours e value")
+    void updateBudgetCase2() {
+        //given
+        var budget = BudgetBuilder.init().builder();
+
+        var dataToUpdate = UpdateBudgetBuilder.init()
+                .withCustomerName(null)
+                .withProject(null)
+                .withProjectStages(null)
+                .withWorkAddress(null)
+                .withDate(null)
+                .builder();
+
+        //when
+        budget.updateBudget(dataToUpdate);
+
+        //then
+        assertThat(budget.getCustomerName()).isEqualTo("Any Name");
+        assertThat(budget.getProject()).isEqualTo("Any Project");
+        assertThat(budget.getProjectStages()).isEqualTo("Any Stages");
+        assertThat(budget.getSquareMeters()).isEqualTo(dataToUpdate.squareMeters().get());
+        assertThat(budget.getWorkAddress()).isEqualTo("Any Address");
+        assertThat(budget.getEstimedHours()).isEqualTo(dataToUpdate.estimedHours().get());
+        assertThat(budget.getDate()).isEqualTo(LocalDate.now().plusDays(1));
+        assertThat(budget.getValue()).isEqualTo(dataToUpdate.value().get());
+    }
+
+
 }
