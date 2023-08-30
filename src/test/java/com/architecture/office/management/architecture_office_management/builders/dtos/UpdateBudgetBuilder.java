ff5cd70202686003138f882aec24535cade70363
Carla Hentschel <carlarhentschel@gmail.com>
2023-08-30 12:54:08 -0300

    atribui novo valor para parâmetro 'date' e adiciona mais métodos

--- src/test/java/com/architecture/office/management/architecture_office_management/builders/dtos/UpdateBudgetBuilder.java
+++ src/test/java/com/architecture/office/management/architecture_office_management/builders/dtos/UpdateBudgetBuilder.java
@@ -1,7 +1,6 @@
 package com.architecture.office.management.architecture_office_management.builders.dtos;
 
 import com.architecture.office.management.architecture_office_management.dtos.UpdateBudget;
-
 import java.time.LocalDate;
 import java.util.Optional;
 
@@ -13,7 +12,7 @@ public class UpdateBudgetBuilder {
     private int squareMeters =  222;
     private String workAddress = "Any Address Updated";
     private int estimedHours = 22;
-    private LocalDate date = LocalDate.of(2023,8,24);
+    private LocalDate date =  LocalDate.now().plusDays(2);
     private double value = 2222.22;
 
     public static UpdateBudgetBuilder init() {
@@ -32,4 +31,30 @@ public class UpdateBudgetBuilder {
                 Optional.of(value)
         );
     }
+
+    public UpdateBudgetBuilder withCustomerName(String customerName) {
+        this.customerName = customerName;
+        return this;
+    }
+
+    public UpdateBudgetBuilder withProject(String project) {
+        this.project = project;
+        return this;
+    }
+
+    public UpdateBudgetBuilder withProjectStages(String projectStages) {
+        this.projectStages = projectStages;
+        return this;
+    }
+
+    public UpdateBudgetBuilder withWorkAddress(String workAddress) {
+        this.workAddress = workAddress;
+        return this;
+    }
+
+    public UpdateBudgetBuilder withDate(LocalDate date) {
+        this.date = date;
+        return this;
+    }
+
 }
